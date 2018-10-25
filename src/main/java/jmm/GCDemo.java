a1a41d943681dec93e3bd9c1c2a5616f2f72e2e3
Paweł Beder <pawel.beder@gmail.com>
2018-10-25 20:26:26 +0200

    Added GCDemo.

---
+++ src/main/java/jmm/GCDemo.java
@@ -0,0 +1,34 @@
+package jmm;
+
+import java.util.Random;
+
+public class GCDemo {
+
+    private static final int ONE_MILLION = 1_000_000;
+
+    public static void main(String[] args) throws InterruptedException {
+        SpaceWasted[] wasted = new SpaceWasted[ONE_MILLION];
+
+        int count = 0;
+        Random rnd = new Random();
+        while (true) {
+            wasted[rnd.nextInt(wasted.length)] = new SpaceWasted();
+            if (count % ONE_MILLION == 0) {
+                System.out.println("*");
+            }
+            count++;
+            Thread.sleep(1);
+        }
+    }
+}
+
+class SpaceWasted{
+    long a;
+    long b;
+    long c;
+    long d;
+    long e;
+    long f;
+    long g;
+    long h;
+}
